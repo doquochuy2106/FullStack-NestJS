1e812ad8115fbca04c443d3c757390b1153e987e
QuocHuy <quochuy@phutungb2b.com>
2026-04-16 16:54:15 +0700

    7. Tích hợp Passport với Nestjs | Tự Học Fullstack Next.js/Nest.js với TypeScript

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,14 @@ import { UsersService } from '@/modules/users/users.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { UsersModule } from '@/modules/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './passport/local.strategy';
+import { JwtStrategy } from './passport/jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule,
     JwtModule.registerAsync({
       useFactory: async (configService: ConfigService) => ({
         global: true,
@@ -21,7 +25,7 @@ import { UsersModule } from '@/modules/users/users.module';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,16 +10,33 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
     const isValidPassword = await comparePasswordHelper(pass, user.password);
-    if (!isValidPassword) {
-      throw new UnauthorizedException('Username/Password không chính xác');
+    if (!user || !isValidPassword) {
+      return null;
+    } else {
+      return user;
     }
+  }
 
+  async login(user: any) {
     const payload = { sub: user._id, username: user.email };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
   }
+
+  // async signIn(username: string, pass: string): Promise<any> {
+  //   const user = await this.usersService.findByEmail(username);
+  //   const isValidPassword = await comparePasswordHelper(pass, user.password);
+  //   if (!isValidPassword) {
+  //     throw new UnauthorizedException('Username/Password không chính xác');
+  //   }
+
+  //   const payload = { sub: user._id, username: user.email };
+  //   return {
+  //     access_token: await this.jwtService.signAsync(payload),
+  //   };
+  // }
 }
