248d9eb841ad66ff3f44c88e5f4f9a6b25ef310a
QuocHuy <quochuy@phutungb2b.com>
2026-04-24 17:09:52 +0700

    15. Protected Page với Next.js | Tự Học Fullstack Next.js/Nest.js với TypeScript

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,7 +15,7 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './passport/local-auth.guard';
 import { JwtAuthGuard } from './passport/jwt-auth.guard';
-import { Public } from '@/decorator/customize';
+import { Public, ResponseMessage } from '@/decorator/customize';
 import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('auth')
@@ -27,6 +27,7 @@ export class AuthController {
 
   @Post('login')
   @Public()
+  @ResponseMessage('Fetch Login')
   @UseGuards(LocalAuthGuard)
   async handleLogin(@Request() req) {
     return this.authService.login(req.user);
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,20 +13,21 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
+    if (!user) return null;
+
     const isValidPassword = await comparePasswordHelper(pass, user.password);
-    if (!user || !isValidPassword) {
-      return null;
-    } else {
-      return user;
-    }
+    if (!isValidPassword) return null;
+    return user;
   }
 
   async login(user: any) {
     const payload = { sub: user._id, username: user.email };
     return {
-      _id: user._id,
-      email: user.email,
-      name: user.name,
+      user: {
+        _id: user._id,
+        email: user.email,
+        name: user.name,
+      },
       access_token: await this.jwtService.signAsync(payload),
     };
   }
